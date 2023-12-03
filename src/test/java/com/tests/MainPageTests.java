47a1da3a7b5b082c4fe1fbbc7fe339ed10339ffa
Borys Pedorenko <boriszhenya@gmail.com>
2023-12-03 19:13:03 +0100

    HW 03/12/2023

--- src/test/java/com/tests/MainPageTests.java
+++ src/test/java/com/tests/MainPageTests.java
@@ -1,6 +1,8 @@
 package com.tests;
 
+import com.pages.JacketPage;
 import com.pages.LoginPage;
+import com.pages.MainPage;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,6 +16,26 @@ public class MainPageTests extends TestBase {
                         .loginAsStandardUser()
                         .clickAllAddToCartButtons()
                         .getShoppingCartItemsAmount()
+        );
+    }
+
+    @Test
+    public void testGoToPageJacket() {
+        assertEquals(
+                "$49.99",
+                new LoginPage(context).
+                        loginAsStandardUser().
+                        goToPageJacket().
+                        priceJacketText()
+        );
+    }
+
+    @Test
+    public void shoppingCartQuantity() {
+        assertEquals(
+                "1",
+                new LoginPage(context).loginAsStandardUser().addToCartByIndex(0).numberOfItemsInTheCart()
+
         );
     }
 }
